6b9dc3f9ff9582de566bf06fead97dcd026939b2
glennbech <glenn.bech@gmail.com>
2020-10-25 11:30:08 +0100

    Logs on ping and more

--- src/main/java/no/pgr301/travisdemo/account/AccountsController.java
+++ src/main/java/no/pgr301/travisdemo/account/AccountsController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 public class AccountsController {
 
     private AccountRepository accountRepository;
+    private static final Logger LOG = Logger.getLogger(AccountsController.class.getName());
 
     @Autowired
     public AccountsController(AccountRepository accountRepository) {
@@ -19,6 +21,7 @@ public class AccountsController {
 
     @RequestMapping(path = "/ping")
     public String ping() {
+        LOG.info("Ping");
         return "pong";
     }
 
@@ -29,7 +32,7 @@ public class AccountsController {
 
     @RequestMapping(path = "/accounts/{ssn}")
     public List<Account> getAccounts(@PathVariable("ssn") String ssn) {
+        LOG.info(String.format("Account list requested for %s", ssn));
         return accountRepository.findBySocialSecurityNumber(ssn);
     }
-
 }
